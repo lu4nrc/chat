7d366c6ccf1f248fc6c9206bbe437e9f55c9ae3e
lu4nrc <luan.rcardosoo@gmail.com>
2024-04-15 22:44:04 -0300

    update scheduled service

--- backend/src/services/ScheduleService/NotificationScheduledService.ts
+++ backend/src/services/ScheduleService/NotificationScheduledService.ts
@@ -1,10 +1,11 @@
 import Scheduled from "../../models/Scheduled";
 import { Op } from "sequelize";
 import ListWhatsAppsService from "../WhatsappService/ListWhatsAppsService";
+import { parseEndDate, parseInitialDate } from "../../utils/parserdate";
 import FindOrCreateTicketService from "../TicketServices/FindOrCreateTicketService";
 import SendWhatsAppMessage from "../WbotServices/SendWhatsAppMessage";
 import Ticket from "../../models/Ticket";
-
+import { format } from "date-fns";
 const NotificationScheduledService = async (): Promise<void> => {
   var data = new Date();
   data.setMilliseconds(0);
@@ -19,18 +20,25 @@ const NotificationScheduledService = async (): Promise<void> => {
     e => e.isDefault === true
   );
   scheduleds.map(scheduled => {
-    var phrase = `\n*LEMBRETE* \n\n*Título:* ${scheduled.title}\n*Anfitrião:* ${
+    const initDate = format(scheduled.startDate, "dd/MM/yyyy");
+    const finishDate = format(scheduled.endDate, "dd/MM/yyyy");
+
+    const initHours = format(scheduled.startDate, "HH:mm");
+    const finishHours = format(scheduled.endDate, "HH:mm");
+
+    var phrase = `*AGENDAMENTO* \n *Título:* ${
+      scheduled.title
+    }\n\n \u{1F4C6} *Data:* ${
+      initDate === finishDate ? initDate : `${initDate} até ${finishDate}`
+    }\n \u{1F553} *Horário:* ${initHours}h às ${finishHours}h \n\n *Anfitrião:* ${
       scheduled.anfitriao.name
-    }\n*Data:* ${parseInitialDate(
-      new Date(scheduled.startDate)
-    )} às ${parseEndDate(new Date(scheduled.endDate))}\n*Local:* ${
-      scheduled.typeEvent === 1 ? "Evento" : "online"
-    }  ${scheduled.locale}\n*Participantes:* ${scheduled.externals.map(
+    } \n *${scheduled.typeEvent === 1 ? "Local" : "Online"}:* ${
+      scheduled.locale
+    } \n *Participantes:* ${scheduled.externals.map(
       e => e.name + ", "
-    )}${scheduled.attendants.map(i => " " + i.name)}\n*Descrição:* ${
+    )}${scheduled.attendants.map(i => " " + i.name)} \n *Descrição:* ${
       scheduled.description
-    }
-   `;
+    }`;
     scheduled.notificationType.map(async (e: number) => {
       if (e === 1) {
         await Promise.all(
