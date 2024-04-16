9f1815abc23a85c77a930858ffc2eb1db1cfff84
lu4nrc <luan.rcardosoo@gmail.com>
2024-04-16 10:41:02 -0300

    pre-fix MessageList

--- frontend/src/components/MessagesList/index.js
+++ frontend/src/components/MessagesList/index.js
@@ -420,7 +420,6 @@ const MessagesList = ({ ticketId, isGroup }) => {
                 pr={"32px"}
                 minHeight={"38px"}
                 marginBottom={1}
-                width={message.mediaUrl ? "220px" : "fit-content"}
                 maxWidth={{ xs: "100%", md: "75%" }}
                 overflow={"hidden"}
                 wordWrap="break-word"
