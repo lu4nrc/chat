1e478ee15447f9485fb63c4ebf9c55f8810904c8
Luan R <luan.rcardosoo@gmail.com>
2025-10-17 09:11:20 -0300

    comment: CloseTicketAuto

--- frontend/src/hooks/useTickets/index.js
+++ frontend/src/hooks/useTickets/index.js
@@ -1,12 +1,85 @@
-import { useState, useEffect } from "react";
-import { getHoursCloseTicketsAuto } from "../../config";
-import toastError from "../../errors/toastError";
+import { useState, useEffect } from 'react';
+import { getHoursCloseTicketsAuto } from '../../config';
+import toastError from '../../errors/toastError';
 
-import api from "../../services/api";
-import { useOutletContext } from "react-router-dom";
-import { useToast } from "../use-toast";
+import api from '../../services/api';
+import { useOutletContext } from 'react-router-dom';
+import { useToast } from '../use-toast';
 
 const useTickets = ({
+  searchParam,
+  pageNumber,
+  status,
+  date,
+  showAll,
+  queueIds,
+  withUnreadMessages,
+}) => {
+  const [loading, setLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(false);
+  const [tickets, setTickets] = useState([]);
+  const [count, setCount] = useState(0);
+  const { toast } = useToast();
+
+  useEffect(() => {
+    setLoading(true);
+    const delayDebounceFn = setTimeout(() => {
+      const fetchTickets = async () => {
+        try {
+          const { data } = await api.get('/tickets', {
+            params: {
+              searchParam,
+              pageNumber,
+              status,
+              date,
+              showAll,
+              queueIds,
+              withUnreadMessages,
+            },
+          });
+
+          setTickets(data.tickets);
+
+          // let horasFecharAutomaticamente = getHoursCloseTicketsAuto();
+
+          // if (status === "open" && horasFecharAutomaticamente && horasFecharAutomaticamente !== "" &&
+          //     horasFecharAutomaticamente !== "0" && Number(horasFecharAutomaticamente) > 0) {
+
+          //     let dataLimite = new Date()
+          //     dataLimite.setHours(dataLimite.getHours() - Number(horasFecharAutomaticamente))
+
+          //     data.tickets.forEach(ticket => {
+          //         if (ticket.status !== "closed") {
+          //             let dataUltimaInteracaoChamado = new Date(ticket.updatedAt)
+          //             if (dataUltimaInteracaoChamado < dataLimite)
+          //                 closeTicket(ticket)
+          //         }
+          //     })
+          // }
+
+          setHasMore(data.hasMore);
+          setCount(data.count);
+          setLoading(false);
+        } catch (err) {
+          setLoading(false);
+          toast({
+            variant: 'destructive',
+            title: toastError(err),
+          });
+        }
+      };
+
+      const closeTicket = async (ticket) => {
+        await api.put(`/tickets/${ticket.id}`, {
+          status: 'closed',
+          userId: ticket.userId || null,
+        });
+      };
+
+      fetchTickets();
+    }, 500);
+    return () => clearTimeout(delayDebounceFn);
+  }, [
     searchParam,
     pageNumber,
     status,
@@ -14,82 +87,9 @@ const useTickets = ({
     showAll,
     queueIds,
     withUnreadMessages,
-}) => {
-    const [loading, setLoading] = useState(true);
-    const [hasMore, setHasMore] = useState(false);
-    const [tickets, setTickets] = useState([]);
-    const [count, setCount] = useState(0);
-    const { toast } = useToast()
+  ]);
 
-    useEffect(() => {
-        setLoading(true);
-        const delayDebounceFn = setTimeout(() => {
-            const fetchTickets = async () => {
-                try {
-                    const { data } = await api.get("/tickets", {
-                        params: {
-                            searchParam,
-                            pageNumber,
-                            status,
-                            date,
-                            showAll,
-                            queueIds,
-                            withUnreadMessages,
-                        },
-                    })
-                   
-                    setTickets(data.tickets)
-
-                    let horasFecharAutomaticamente = getHoursCloseTicketsAuto();
-
-                    if (status === "open" && horasFecharAutomaticamente && horasFecharAutomaticamente !== "" &&
-                        horasFecharAutomaticamente !== "0" && Number(horasFecharAutomaticamente) > 0) {
-
-                        let dataLimite = new Date()
-                        dataLimite.setHours(dataLimite.getHours() - Number(horasFecharAutomaticamente))
-
-                        data.tickets.forEach(ticket => {
-                            if (ticket.status !== "closed") {
-                                let dataUltimaInteracaoChamado = new Date(ticket.updatedAt)
-                                if (dataUltimaInteracaoChamado < dataLimite)
-                                    closeTicket(ticket)
-                            }
-                        })
-                    }
-
-                    setHasMore(data.hasMore)
-                    setCount(data.count)
-                    setLoading(false)
-                } catch (err) {
-                    setLoading(false)
-                    toast({
-                        variant: "destructive",
-                        title: toastError(err),
-                      });
-                }
-            }
-
-            const closeTicket = async (ticket) => {
-                await api.put(`/tickets/${ticket.id}`, {
-                    status: "closed",
-                    userId: ticket.userId || null,
-                })
-            }
-
-            fetchTickets()
-        }, 500)
-        return () => clearTimeout(delayDebounceFn)
-    }, [
-        searchParam,
-        pageNumber,
-        status,
-        date,
-        showAll,
-        queueIds,
-        withUnreadMessages,
-    ])
-
-    return { tickets, loading, hasMore, count };
+  return { tickets, loading, hasMore, count };
 };
 
 export default useTickets;
