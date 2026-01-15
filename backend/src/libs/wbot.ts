f93809484eb52dd03770724b78006d0569af927f
Luan R <luan.rcardosoo@gmail.com>
2026-01-15 19:18:08 +0700

    update wbot

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -47,67 +47,25 @@ export const initWbot = async (whatsapp: Whatsapp): Promise<Session> => {
       if (whatsapp.session) {
         sessionCfg = JSON.parse(whatsapp.session);
       }
+      const args: string = process.env.CHROME_ARGS || "";
 
       const wbot: Session = new Client({
         session: sessionCfg,
-        authStrategy: new LocalAuth({ clientId: "bd_" + whatsapp.id }),
-        restartOnAuthFail: true,
-        qrMaxRetries: 4,
-        webVersionCache: {
-          type: "remote",
-          remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/refs/heads/main/html/2.3000.1031490220-alpha.html`
-        },
+        authStrategy: new LocalAuth({ clientId: `bd_${whatsapp.id}` }),
         puppeteer: {
-          //executablePath: "/usr/bin/google-chrome",
-          executablePath: "/usr/bin/chromium-browser",
-          headless: true,
+          // headless: false, // TODO make sure chromium closes on session disconnection / delete
+          executablePath: process.env.CHROME_BIN || undefined,
+          browserWSEndpoint: process.env.CHROME_WS || undefined,
           args: [
-            "--autoplay-policy=user-gesture-required",
-            "--disable-background-networking",
-            "--disable-background-timer-throttling",
-            "--disable-backgrounding-occluded-windows",
-            "--disable-breakpad",
-            "--disable-client-side-phishing-detection",
-            "--disable-component-update",
-            "--disable-default-apps",
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
             "--disable-dev-shm-usage",
-            "--disable-domain-reliability",
-            "--disable-extensions",
-            "--disable-features=AudioServiceOutOfProcess",
-            "--disable-hang-monitor",
-            "--disable-ipc-flooding-protection",
-            "--disable-notifications",
-            "--disable-offer-store-unmasked-wallet-cards",
-            "--disable-popup-blocking",
-            "--disable-print-preview",
-            "--disable-prompt-on-repost",
-            "--disable-renderer-backgrounding",
-            "--disable-speech-api",
-            "--disable-sync",
-            "--disable-gpu",
             "--disable-accelerated-2d-canvas",
-            "--hide-scrollbars",
-            "--ignore-gpu-blacklist",
-            "--metrics-recording-only",
-            "--mute-audio",
-            "--no-default-browser-check",
             "--no-first-run",
-            "--no-pings",
             "--no-zygote",
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
             "--disable-gpu",
-            "--no-zygote",
-            "--single-process",
-            "--password-store=basic",
-            "--use-gl=swiftshader",
-            "--use-mock-keychain",
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--disable-blink-features=AutomationControlled"
-          ],
-          browserWSEndpoint: process.env.CHROME_WS || undefined
+            ...args.split(" ")
+          ]
         }
       });
 
